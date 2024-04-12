de3141fd13cddcc09f9c597c4cf125c6b918ac24
gabriel-mrb <163215540+gabriel-mrb@users.noreply.github.com>
2024-04-12 11:26:48 -0300

    exercicio7.py

--- Lista pratica 4 (listas e estruturas de repetição)/exercicio7.py
+++ Lista pratica 4 (listas e estruturas de repetição)/exercicio7.py
@@ -1,11 +1,11 @@
 # Faça um programa que calcule e mostre a soma dos 50 primeiros
 # números pares
 
-pares_for=[]
-pares_while=[]
+pares=[]
+
 
 for i in range(0,101):
     if i%2==0:
-         pares_for.append(i)
+         pares.append(i)
 print(80*"-")
-print(f"A soma dos 50 primeiros pares é de: {sum(pares_for)}")
+print(f"A soma dos 50 primeiros pares é de: {sum(pares)}")
